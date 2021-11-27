94f920ef7c70fd251326bff7859488cc886729a1
lismagilova <91222024+lismagilova@users.noreply.github.com>
2021-11-27 14:47:43 +0300

    Update triangle 6.11.py

--- triangle 6.11.py
+++ triangle 6.11.py
@@ -1,22 +1,41 @@
-class Triangle():
-    def init(self, a, b):
-        self.a = a
-        self.b = b
+import math
 
-    def calc_hypothesis(self):
-        self.c = (self.a + self.b) ** 0.5
-        return self.c
+class Trapezoid():
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
 
-    def calc_p(self):
-        calc_hypothesis(self)
-        return self.a + self.b + self.c
+    def side(self):
+        c = math.sqrt(((self.x2 - self.x1) ** 2) + ((self.y2 - self.y1) ** 2))
+        a = math.sqrt(((self.x3 - self.x2) ** 2) + ((self.y3 - self.y2) ** 2))
+        b = math.sqrt(((self.x1 - self.x3) ** 2) + ((self.y1 - self.y3) ** 2))
+        print("Длина сторон: " + "\nAB: ", c, "\nBC: ", a, "\nCA: ", b)
 
-    def calc_s(self):
-        return self.a * self.b * 0.5
+    def per(self):
+        c = math.sqrt(((self.x2 - self.x1) ** 2) + ((self.y2 - self.y1) ** 2))
+        a = math.sqrt(((self.x3 - self.x2) ** 2) + ((self.y3 - self.y2) ** 2))
+        b = math.sqrt(((self.x1 - self.x3) ** 2) + ((self.y1 - self.y3) ** 2))
+        P = a + b + c
+        print("Периметр:", P)
+
+    def area(self):
+        c = math.sqrt(((self.x2 - self.x1) ** 2) + ((self.y2 - self.y1) ** 2))
+        a = math.sqrt(((self.x3 - self.x2) ** 2) + ((self.y3 - self.y2) ** 2))
+        b = math.sqrt(((self.x1 - self.x3) ** 2) + ((self.y1 - self.y3) ** 2))
+        p = (a + b + c)/ 2
+        S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+        print("Площадь:", S, "\n")
 
 
-
-triangle_1 = Triangle(2, 3)
-print(calc_hypothesis(triangle_1))
-print(calc_p(triangle_1))
-print(calc_s(triangle_1))
+fis_trian = Trapezoid(1, 2, 5, 3, 7, 4)
+fis_trian.side()
+fis_trian.per()
+fis_trian.area()
+# fis_trian1 = Trapezoid(0, 0, 5, 5, 7, 4)
+# fis_trian1.side()
+# fis_trian1.per()
+# fis_trian1.area()
