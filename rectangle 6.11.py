b0f5d93ae3976035a5d00c69a1d03194e484e974
lismagilova <91222024+lismagilova@users.noreply.github.com>
2021-11-27 14:58:52 +0300

    Add files via upload

---
+++ rectangle 6.11.py
@@ -0,0 +1,41 @@
+class Rectangle():
+    def __init__(self, x1, y1, x2, y2, x3, y3, x4, y4):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+        self.x4 = x4
+        self.y4 = y4
+
+    def side(self):
+        a = self.x2 - self.x1
+        b = self.y2 - self.y3
+        c = self.x3 - self.x4
+        d = self.y1 - self.y4
+        print("Длина сторон: " + "\nAB: ", a, "\nBC: ", c, "\nCD: ", d, "\nDC: ", b)
+
+    def per(self):
+        a = self.x2 - self.x1
+        b = self.y2 - self.y3
+        c = self.x3 - self.x4
+        d = self.y1 - self.y4
+        P = a + b + c + d
+        print("Периметр:", P)
+
+    def area(self):
+        a = self.x2 - self.x1
+        b = self.y2 - self.y3
+        S = a * b
+        print("Площадь:", S, "\n")
+
+
+fis_rec = Rectangle(5, 4, 6, 4, 6, 2, 5, 2)
+fis_rec.side()
+fis_rec.per()
+fis_rec.area()
+# fis_rec1 = Rectangle(0, 0, 3, 3, 6, 3, 9, 0)
+# fis_rec1.side()
+# fis_rec1.per()
+# fis_rec1.area()
